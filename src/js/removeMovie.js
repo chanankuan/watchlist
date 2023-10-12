d181c68683eb6a3c1fbb230c35c1c95e6d090020
Anton Chan <106675165+chanchik96@users.noreply.github.com>
2023-10-12 22:42:32 +0300

    Change path.

--- src/js/removeMovie.js
+++ src/js/removeMovie.js
@@ -1,8 +1,8 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { KEY_LS } from './refs';
-import { setToLocalStorage } from './helper';
+import { setToLocalStorage } from '../utils/appUtils';
 
-export default function removeFromfavorite(event) {
+export function removeFromfavorite(event) {
   let watchList = JSON.parse(localStorage.getItem(KEY_LS)) || [];
   const movieId = event.target.dataset.id;
 
